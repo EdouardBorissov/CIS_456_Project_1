ae4bd15e132ae77b6e75712419e8d245d9182c91
namcn1namcn <namcn@live.com>
2019-09-20 16:30:15 -0500

    Added Scripts

---
+++ Assets/Scripts/Player/Player_Damage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Damage : MonoBehaviour
+{
+    private float currentHealth;
+    public float maxHealth = 100f;
+    public Image healthBar;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.fillAmount = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            currentHealth -= 20;
+            healthBar.fillAmount = currentHealth / maxHealth;
+        }
+
+        if(currentHealth <= 0)
+        {
+            Destroy(player);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player_Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jump = 5f;
+    private bool onGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) && onGround)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            onGround = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+            onGround = true;
+    }
+}
