40a484e794cc49b7df6611c427d0b7131fe452b6
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-09-20 15:15:02 -0500

    Bullet reflection works now

--- Assets/Scripts/Objects/BulletScript.cs
+++ Assets/Scripts/Objects/BulletScript.cs
@@ -13,32 +13,25 @@ public class BulletScript : MonoBehaviour
      */
 
     private float bulletSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Wall")
         {
-            
             //grab the bullet's rigidbody
-            Rigidbody2D bulletRB = gameObject.GetComponent<Rigidbody2D>();
+          Rigidbody2D bulletRB = gameObject.GetComponent<Rigidbody2D>();
 
             Debug.Log("Old Velocity " + bulletRB.velocity.x.ToString() + " " + bulletRB.velocity.y.ToString());
             //change the velocity based on where this wall is facing
-            bulletRB.velocity = (Vector3.Reflect(bulletRB.velocity, collision.contacts[0].normal).normalized) * bulletSpeed * Time.deltaTime;
+
+            bulletRB.velocity = (Vector2.Reflect(bulletRB.velocity, collision.contacts[0].normal).normalized) * bulletSpeed * Time.deltaTime;
 
             Debug.Log("New Velocity " + bulletRB.velocity.x.ToString() + " " + bulletRB.velocity.y.ToString());
 
+        }
+        else if(!collision.gameObject.CompareTag("Wall") && !collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
         }
     }
 
--- Assets/Scripts/Player/Player_Shoot.cs
+++ Assets/Scripts/Player/Player_Shoot.cs
@@ -55,7 +55,7 @@ public class Player_Shoot : MonoBehaviour
                 GameObject firedBullet = Instantiate(revolverBullet, revolverMuzzle.transform.position, revolverMuzzle.transform.rotation);//Spawns bullet.
                 firedBullet.GetComponent<Rigidbody2D>().velocity = firedBullet.transform.right * revolverBulletSpeed * Time.deltaTime;
                 firedBullet.GetComponent<BulletScript>().SetBulletSpeed(revolverBulletSpeed);
-                Destroy(firedBullet, revolverBulletLifeTime);
+               Destroy(firedBullet, revolverBulletLifeTime);
                 if (loadedRevolverAmmo <= 0) // If the player tries to shoot, auto-reload.
                 {
                    Invoke("ReloadRevolver", revolverReloadTime);
