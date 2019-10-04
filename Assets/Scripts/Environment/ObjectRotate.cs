bd860246bc5a90382642dd1d736d35f6183ce6a6
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-10-04 16:22:18 -0500

    poosh

---
+++ Assets/Scripts/Environment/ObjectRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotate : MonoBehaviour
+{
+
+    public float rotationAmount;
+    public float rotationFrequency;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        InvokeRepeating("RotateObject", 0, rotationFrequency);
+    }
+
+
+    public void RotateObject()
+    {
+        gameObject.transform.Rotate(0, 0, rotationAmount);
+    }
+}
