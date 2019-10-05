2f16d781ec8870e48530d62becc62ebe536bdb04
Warren <guileswa@gmail.com>
2019-10-04 20:13:58 -0500

    Mergearoni and Cheese

--- Assets/Scripts/Objects/BulletScript.cs
+++ Assets/Scripts/Objects/BulletScript.cs
@@ -46,11 +46,37 @@ public class BulletScript : MonoBehaviour
             }
 
         }
+        else if (collision.gameObject.CompareTag("Rust"))
+        {
+
+            Destroy(collision.gameObject);
+                        //grab the bullet's rigidbody
+          Rigidbody2D bulletRB = gameObject.GetComponent<Rigidbody2D>();
+
+          //  Debug.Log("Old Velocity " + bulletRB.velocity.x.ToString() + " " + bulletRB.velocity.y.ToString());
+            
+            //change the velocity based on where this wall is facing
+
+            bulletRB.velocity = (Vector2.Reflect(bulletRB.velocity, collision.contacts[0].normal).normalized) * (bulletSpeed/2) * Time.deltaTime;
+
+           // Debug.Log("New Velocity " + bulletRB.velocity.x.ToString() + " " + bulletRB.velocity.y.ToString());
+
+            SoundManager.instance.PlayRandomRicochet();
+
+            numberOfBounces++;
+
+            if (numberOfBounces > bounceLimit && bounceLimit != 0)
+            {
+                Destroy(gameObject);
+            }
+        }
         //destroy bullets that hit the player
         else if(!collision.gameObject.CompareTag("Wall") && !collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
+
+
     }
 
     //setter for bullet speed
