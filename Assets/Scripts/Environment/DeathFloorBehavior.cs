12455eb1814c018a7924474ec08e924312d737d4
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-11-01 15:28:53 -0500

    DeathFloor

---
+++ Assets/Scripts/Environment/DeathFloorBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ Eddie B
+     */
+
+public class DeathFloorBehavior : MonoBehaviour
+{
+    public bool canKill = true;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player" && canKill)
+        {
+            CheckpointManager.instance.LoadToLastCheckpoint();
+        }
+    }
+}
