766bf7c2bc53039a10a6264b538f09256e77b94b
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-11-01 22:27:04 -0500

    game loop complete

---
+++ Assets/Scripts/Objects/LevelEnd.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelEnd : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("conversation.start:" );
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("conversation.end");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
