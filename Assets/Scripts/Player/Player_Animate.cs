2dfe606e86ca737535e4e54207dac53f60b5d31c
Connor <46971709+cmwolf19@users.noreply.github.com>
2019-11-06 15:24:38 -0600

    Walk Cycle

---
+++ Assets/Scripts/Player/Player_Animate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Animate : MonoBehaviour
+{
+    Player_Move myMove;
+    Rigidbody2D self;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = GetComponent<Rigidbody2D>();
+        myMove = GetComponent<Player_Move>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Moving", (self.velocity != Vector2.zero));
+        anim.SetBool("Airborne", !myMove.onGround);
+    }
+}
