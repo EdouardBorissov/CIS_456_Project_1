5bc2ca0ea39fbde1a788091381e2da85fc28fea6
Connor <46971709+cmwolf19@users.noreply.github.com>
2019-11-13 15:39:13 -0600

    level

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    private AudioSource[] audioSources;
+
+    [Range(0,1)]
+    public float volume;
+    public bool loop;
+
+    private void OnEnable()
+    {
+        GameObject[] sms = GameObject.FindGameObjectsWithTag("SoundManager");
+        if (sms.Length != 1) Destroy(gameObject);
+        
+
+        DontDestroyOnLoad(gameObject);
+        {
+            foreach (AudioClip ac in audioClips)
+            {
+                AudioSource ass = gameObject.AddComponent<AudioSource>();
+                ass.clip = ac;
+            }
+
+            audioSources = GetComponents<AudioSource>();
+            foreach (AudioSource ass in audioSources)
+            {
+                ass.volume = volume;
+                ass.loop = loop;
+            }
+        }
+    }
+    public void PlaySound(string name)
+    {
+        foreach(AudioSource ass in audioSources)
+        {
+            if (ass.clip.name == name)
+            {
+                ass.Play();
+                return;
+            }
+        }
+        Debug.LogWarning("No sound called " + name);
+    }
+
+    public void Shush()
+    {
+        foreach(AudioSource ass in audioSources)
+        {
+            if (ass.isPlaying)
+            {
+                ass.Stop();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
